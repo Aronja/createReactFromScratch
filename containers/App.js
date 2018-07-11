8bf2a00d15d3163dc519738e32efe544384abd53
Aronja <anovicky@thoughtworks.com>
2018-07-11 23:57:27 +0100

    Initial Commit

---
+++ containers/App.js
@@ -0,0 +1,7 @@
+mport React, {Component} from 'react';
+
+export default class App extends Component {
+    render () {
+        return <p>This is my new react app</p>
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+mport React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
